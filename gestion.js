7a69addd9053274b29e37ea40455640ed40ed1ff
alex <55729475+Alex1030617333@users.noreply.github.com>
2024-12-04 16:57:01 -0500

    subiendo modificacion de menu

---
+++ gestion.js
@@ -0,0 +1,20 @@
+const agregar = document.getElementById("agregar")
+const agregar_menu = document.getElementById("agregar_menu").addEventListener("click",ver_menu)
+
+function ver_menu() {
+        if (agregar.style.display=="none" )
+            agregar.style.display= "inline"
+        else{
+            agregar.style.display=="none" 
+        }
+        
+    }
+
+
+// function ver_menu() {
+//     if (prestamos_menu.style.display=="none" || agregar_menu.style.display=="none"  ||ventas_menu.style.display=="none") {
+//         prestamos_menu.style.display=="inline" 
+//         agregar_menu.style.display=="inline"
+//         ventas_menu.style.display=="inline"
+//     }
+// }
