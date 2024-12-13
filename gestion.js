7774d3030b799699bf14803ec17eaa3c54f638ce
alex <55729475+Alex1030617333@users.noreply.github.com>
2024-12-13 12:30:21 -0500

    modificacion

--- gestion.js
+++ gestion.js
@@ -1,7 +1,8 @@
 const agregar = document.getElementById("agregar")
 const agregar_menu = document.getElementById("agregar_menu")
+const prestamos_menu = document.getElementById("prestamos_menu")
 
-.addEventListener("click",function (e) {
+agregar_menu.addEventListener("click",function (e) {
     e.preventDefault()
     if (agregar.style.display=="none" )
         agregar.style.display= "inline"
@@ -10,6 +11,17 @@ const agregar_menu = document.getElementById("agregar_menu")
     }
 })
 
+
+
+
+prestamos_menu.addEventListener("click",function (e) {
+    e.preventDefault()
+    if (prestamos.style.display=="none" )
+        prestamos.style.display= "inline"
+    else{
+        prestamos.style.display ="none" 
+    }
+})
 
 // function ver_menu() {
 //     if (prestamos_menu.style.display=="none" || agregar_menu.style.display=="none"  ||ventas_menu.style.display=="none") {
